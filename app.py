23588a734fe664ef0e29771c398e6b0093416f72
raja <raja.s.dasgupta@gmail.com>
2022-06-15 18:38:14 +0530

    showing anuva the pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route("/", methods=['GET','POST'])
 def index():
-    return "This is my first flask ML project. Also established the CI/CD pipleline"
+    return "This is my first flask ML project. Also established the CI/CD pipleline. showing anuva how the CICD pipeline works"
 
 
 if __name__ =="__main__":

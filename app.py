e246ed04087ceb0d163e6283a61602729281a0d6
raja <raja.s.dasgupta@gmail.com>
2022-06-18 13:28:02 +0530

    Added logger and exception

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from housing.logger import logging
+from housing.exception import HousingException
 
 app=Flask(__name__)
 
 @app.route("/", methods=['GET','POST'])
 def index():
-    return "This is my first flask ML project. Also established the CI/CD pipleline. showing anuva how the CICD pipeline works"
+    try:
+        raise Exception("We are testing custom exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("We are testing logging module")
+    return "This is my first flask ML project. Also established the CI/CD pipleline."
 
 if __name__ =="__main__":
     app.run(debug=True)

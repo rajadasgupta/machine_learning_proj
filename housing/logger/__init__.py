f68da4b7e4124120ad99b9cf3d2ed3f37b38f141
raja <raja.s.dasgupta@gmail.com>
2022-06-19 10:22:30 +0530

    updated

--- housing/logger/__init__.py
+++ housing/logger/__init__.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import os
 
 LOG_DIR="housing_logs"
-CURRENT_TIME_STAMP=" f{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+CURRENT_TIME_STAMP=f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
 
 LOG_FILE_NAME=f"log_{CURRENT_TIME_STAMP}.log"
 
